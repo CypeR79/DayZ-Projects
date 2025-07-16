7ffefd24e7bf2f19add3353189cb4cef3e7b1d56
Cype <157908293+CypeR79@users.noreply.github.com>
2025-07-16 15:21:00 +0200

    Update init.c

--- Raman/newmissionfiles/init.c
+++ Raman/newmissionfiles/init.c
@@ -5,42 +5,67 @@ void main()
 	if ( ce )
 		ce.InitOffline();
 
-	//DATE RESET AFTER ECONOMY INIT-------------------------
-	int year, month, day, hour, minute;
-	int reset_month = 9, reset_day = 20;
-	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+	/*
+	  [Raman] Weather init
+	   Warning: DO NOT ALTER following values as they are interconnected with Raman weather system!
+	   To ensure correct functionality, it is necessary to include weaher init AFTER the hive init.
+	*/
+	Weather weather = GetGame().GetWeather();
+	weather.MissionWeather( true );
+	weather.GetOvercast().SetLimits( 0.0, 0.30 );
+	weather.GetRain().SetLimits( 0.0, 0.0 );
+	weather.GetFog().SetLimits( 0.0, 0.0 );
+	weather.GetOvercast().SetForecastChangeLimits( 0.1, 0.3 );
+	weather.GetOvercast().SetForecastTimeLimits( 1600, 2100 );
+	weather.GetOvercast().Set( 0.0, 0, 0 );
+	weather.GetRain().Set( 0, 0, 0 );
+	weather.GetFog().Set( 0, 0, 0 );
+	weather.SetWindMaximumSpeed( 30 );
+	weather.SetWindFunctionParams( 0.1, 1.0, 42 );
+	weather.SetStorm( 1, 0.7, 30 );
+	weather.SetRainThresholds( 0.0, 1.0, 0 );
 
-	if ((month == reset_month) && (day < reset_day))
-	{
-		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-	}
-	else
-	{
-		if ((month == reset_month + 1) && (day > reset_day))
-		{
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else
-		{
-			if ((month < reset_month) || (month > reset_month + 1))
-			{
-				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-			}
-		}
+	/*
+	  [Raman] Mission time init
+	   after CE init to determine if storage mission type is outside of the required time-frame
+	*/
+	int year, month, day, hour, minute;
+	GetGame().GetWorld().GetDate( year, month, day, hour, minute );
+    if ( ( month > 9 ) || ( month <= 6 ) )
+    {
+    	year = 2011;
+        month = 9;
+        day = 1;
+		
+		GetGame().GetWorld().SetDate( year, month, day, hour, minute );
 	}
 }
 
 class CustomMission: MissionServer
 {
-
 	void SetRandomHealth(EntityAI itemEnt)
 	{
 		if ( itemEnt )
 		{
-			float rndHlt = Math.RandomFloat( 0.45, 0.65 );
+			float rndHlt = Math.RandomFloat( 0.25, 0.65 );
 			itemEnt.SetHealth01( "", "", rndHlt );
 		}
 	}
+
+	override void OnInit()
+    {
+        super.OnInit();
+
+        // this piece of code is recommended otherwise the event system is switched on automatically and runs from default values
+        if ( m_EventManager )
+        {
+            // min time between events, max time between events, max number of events at the same time
+            //m_EventManager.Run( 900, 2700, 1 );
+            m_EventManager.Run( 900, 2700, 1 );
+            // registering events and their probability
+            m_EventManager.RegisterEvent( Sandstorm, 1.0 );
+        }
+    }
 
 	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
 	{
@@ -61,27 +86,31 @@ class CustomMission: MissionServer
 		float rand;
 
 		itemClothing = player.FindAttachmentBySlotName( "Body" );
+		{
+			itemEnt = player.GetInventory().CreateInInventory("Compass");
+
+            itemEnt = player.GetInventory().CreateInInventory("SodaCan_Pipsi");
+
+            itemEnt = player.GetInventory().CreateInInventory("BandageDressing");
+
+            itemEnt = player.GetInventory().CreateInInventory("TunaCan");
+			
+			itemEnt = player.GetInventory().CreateInInventory("Chemlight_White");			
+			
+		};
 		if ( itemClothing )
 		{
 			SetRandomHealth( itemClothing );
 			
-			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
-			player.SetQuickBarEntityShortcut(itemEnt, 0);
-			
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "TunaCan" );
+						if ( Class.CastTo( itemBs, itemEnt ) )
+				itemBs.SetQuantity( 4 );
+
+			SetRandomHealth( itemEnt );
+
 			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
 			int rndIndex = Math.RandomInt( 0, 4 );
 			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
-			SetRandomHealth( itemEnt );
-			player.SetQuickBarEntityShortcut(itemEnt, 1);
-
-			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
-			if ( rand < 0.35 )
-				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
-			else if ( rand > 0.65 )
-				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
-			else
-				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
-			player.SetQuickBarEntityShortcut(itemEnt, 2);
 			SetRandomHealth( itemEnt );
 		}
 		
@@ -89,7 +118,9 @@ class CustomMission: MissionServer
 		if ( itemClothing )
 			SetRandomHealth( itemClothing );
 		
-		itemClothing = player.FindAttachmentBySlotName( "Feet" );	    
+		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+		if ( itemClothing )
+			SetRandomHealth( itemClothing );
 	}
 };
 
